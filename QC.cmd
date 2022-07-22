0f15aff1ec45dc744919f0c007a994001d50214f
“HadiKhoirudin” <347extreme@gmail.com>
2022-07-22 19:44:38 +0700

    Update V3.0 RC1

--- QC.cmd
+++ QC.cmd
@@ -1,38 +1,3 @@
-::[Bat To Exe Converter]
-::
-::fBE1pAF6MU+EWHzeyGM+LAlbRQC9CGS1CqYg2f364+mIr0BQYO0wdLP/+4CvQA==
-::fBE1pAF6MU+EWHzeyGM+LAlbRQC9CGS1CqYg2cu17OeDwg==
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCqDJHiK8FA/IR9reAuNMHiGN6Ad5Ov04ufKlkISWN4bWbv65oarE88m2GbBRqoc9TRTm8Rs
-::YAwzuBVtJxjWCl3EqQJhSA==
-::ZR4luwNxJguZRRmM9UY5IxJALA==
-::Yhs/ulQjdF65
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF65
-::ZR41oxFsdFKZSTk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpSI=
-::egkzugNsPRvcWATEpSI=
-::dAsiuh18IRvcCxnZtBNQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJjZks0
-::ZQ05rAF9IBncCkqN+0xwdVsHAlTi
-::ZQ05rAF9IAHYFVzEqQIBPRpYTwuPMSuOCboQiA==
-::eg0/rx1wNQPfEVWB+kM9LVsJDDWhXA==
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQIRaChdQRSOOSuPCLkT6+P+/arK4mUcUOsUOKbvug==
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCqDJHiK8FA/IR9reAuNMHiGN6Ad5Ov04ufKlkISWN4ObY7X2buDLa0n5UroOK0F135Ts4YlOCYWewquDg==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 :MainMenu
 set Dialog=***                            SELECT MENU                           ***
